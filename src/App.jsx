39b982478f06aefa187f9bca28d96791a3998cbd
jafarlisona <mi7xxzdth@code.edu.az>
2023-10-31 03:36:40 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Nav from "./assets/Components/Nav";
+import Header from "./assets/Components/Header";
+import Project from "./assets/Components/Project";
+import About from "./assets/Components/About";
+import Contact from "./assets/Components/Contact";
+import Location from "./assets/Components/Location";
+import Footer from "./assets/Components/Footer";
+function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Project />
+      <About />
+      <Contact />
+      <Location />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/Components/Location/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Location() {
+  return (
+    <div className="w3-content w3-padding" style={{ maxWidth: "1564px" }}>
+      {/* Image of location/map */}
+      <div className="w3-container">
+        <img
+          src="https://www.w3schools.com/w3images/map.jpg"
+          className="w3-image"
+          style={{ width: "100%" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Location;
